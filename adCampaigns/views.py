560bb5b5390e17ef0422f280a50694fdfeea9745
Travis Davis <skoolboi434@gmail.com>
2025-07-15 16:37:23 -0400

    Campaign dashboard built

--- adCampaigns/views.py
+++ adCampaigns/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
   return render(request, 'adCampaigns/allCampaigns.html')
 
-def adCampaign(request):
+def adCampaign(request, campaign_id):
   return render(request, 'adCampaigns/singleAdCampaign.html')
 
 def createCampaign(request):
